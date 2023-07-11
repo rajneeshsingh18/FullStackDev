28eccdb161c9746a33ac9cc3a3627ed59fc7c248
rajneeshsingh18 <rajneeshsingh9448@gmail.com>
2023-07-11 20:39:36 +0530

    sending data  or changing the Importance of notes

--- Part 2/Data From the server/axiox_and_promises/src/index.js
+++ Part 2/Data From the server/axiox_and_promises/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+
 // import axios from 'axios'
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-<App />
+<App/>
 );
 
 //fulfilled promise
